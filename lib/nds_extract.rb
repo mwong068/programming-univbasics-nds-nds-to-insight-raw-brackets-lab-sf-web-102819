31c875d83743ce8860da8fc7e91f8e3caf0a3fc0
Megan Wong <mwong068@gmail.com>
2019-10-22 21:53:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,9 +7,10 @@ directors_hash = {}
 total = 0 
  row_index = 0 
  while row_index < nds.length do 
+   inner_len = nds[row_index][:movies].length
   column_index = 0 
   while column_index < nds[row_index].length do 
-    inner_len = nds[row_index][:movies].length
+    
       while column_index < inner_len do 
         total += nds[row_index][:movies][column_index][:worldwide_gross]
         puts total
